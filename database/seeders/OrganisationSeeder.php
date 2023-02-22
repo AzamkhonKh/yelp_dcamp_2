c0343fb15a76456536d8db086940f0deeb35c784
Azamkhon <azamkhon.kh@gmail.com>
2023-02-22 19:05:25 +0500

    fix seeder

--- database/seeders/OrganisationSeeder.php
+++ database/seeders/OrganisationSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Comments;
 use App\Models\Tag;
 use App\Models\Organisation;
+use App\Models\TagOrganisation;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,9 +18,13 @@ class OrganisationSeeder extends Seeder
      */
     public function run()
     {
-        Organisation::factory(100)->create()->each(function (Organisation $o){
+        $tags = Tag::factory(10)->create();
+        Organisation::factory(100)->create()->each(function (Organisation $o) use ($tags){
             $o->comments()->saveMany(Comments::factory(10)->make());
-            $o->tags()->saveMany(Tag::factory(10)->make());
+            $count = rand(1, 10);
+            $tags->take($count)
+                ->map(fn (Tag $t) => TagOrganisation::create(['tag_id' => $t->id, 'organisation_id' => $o->id]));
+
         });
     }
 }
